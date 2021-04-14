9d8e6e7f3554f95802a28cae3e2d7d59a5894699
Basharat Hussain Bhat <basharat.hussaini110@gmail.com>
2021-04-14 13:53:05 +0530

    Add project files.

---
+++ ReportingEngine/Controls/VisualTargetPresentationSource.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows;
+
+namespace ReportEngine.Controls
+{
+    /// <summary>
+    /// Visual target presentation source
+    /// </summary>
+    public class VisualTargetPresentationSource : PresentationSource
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="hostVisual">host visual</param>
+        public VisualTargetPresentationSource(HostVisual hostVisual)
+        {
+            _visualTarget = new VisualTarget(hostVisual);
+            AddSource();
+        }
+
+        private bool _isDisposed;
+        /// <summary>
+        /// Object is disposed
+        /// </summary>
+        public override bool IsDisposed
+        {
+            get { return _isDisposed; }
+        }
+
+        public Size DesiredSize { get; protected set; }
+
+        private VisualTarget _visualTarget;
+        /// <summary>
+        /// Root visual
+        /// </summary>
+        public override Visual RootVisual
+        {
+            get { return _visualTarget.RootVisual; }
+            set
+            {
+                Visual oldRoot = _visualTarget.RootVisual;
+                _visualTarget.RootVisual = value;
+
+                RootChanged(oldRoot, value);
+
+                UIElement rootElement = value as UIElement;
+                if (rootElement != null)
+                {
+                    rootElement.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                    rootElement.Arrange(new Rect(rootElement.DesiredSize));
+
+                    DesiredSize = rootElement.DesiredSize;
+                    return;
+                }
+
+                DesiredSize = new Size(0, 0);
+            }
+        }
+
+        /// <summary>
+        /// Gets the composition target core
+        /// </summary>
+        /// <returns></returns>
+        protected override CompositionTarget GetCompositionTargetCore()
+        {
+            return _visualTarget;
+        }
+
+        /// <summary>
+        /// Internal dispose method
+        /// </summary>
+        internal void Dispose()
+        {
+            if (_isDisposed) return;
+
+            RemoveSource();
+            _isDisposed = true;
+        }
+    }
+}
